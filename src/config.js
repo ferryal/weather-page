256e8f9e717931320165f7c4120eb1131da5f74a
ferryal <ferryal.bahari@amartha.com>
2020-07-02 20:30:59 +0700

    add config

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const Config = {
+    apiUrl: 'http://api.ipstack.com',
+    apiUrlWather: 'https://api.openweathermap.org/data/2.5',
+    apiKeyWeather: '95611c5aee12a6c0aa524eb79dffa7bf',
+    apiKey: 'cecdb37dc39168bdfcdda6ba991f5e2b'  
+};
+
+module.exports = Config;

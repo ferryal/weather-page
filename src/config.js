7453d13c8ad98b440db99018d8b9454a3d37939f
ferryal <ferryal.bahari@amartha.com>
2020-07-03 20:40:56 +0700

    change config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const Config = {
     apiUrl: 'http://api.ipstack.com',
-    apiUrlWather: 'https://api.openweathermap.org/data/2.5',
+    apiUrlWeather: 'https://api.openweathermap.org/data/2.5',
     apiKeyWeather: '95611c5aee12a6c0aa524eb79dffa7bf',
     apiKey: 'cecdb37dc39168bdfcdda6ba991f5e2b'  
 };

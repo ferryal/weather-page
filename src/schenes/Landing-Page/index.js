cc507408c4e49cf1c7b06cee1df88855d6de8b10
ferryal <ferryal.bahari@amartha.com>
2020-07-04 21:04:03 +0700

    add components

--- src/schenes/Landing-Page/index.js
+++ src/schenes/Landing-Page/index.js
@@ -1,7 +1,12 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Navbar from './components/Navigation-Bar';
 import WidgetBoard from './components/Widget-Boarding';
 import WidgetWeather from './components/Widget-Weather';
+import WidgetFeatures from './components/Widget-Features';
+import WidgetFooter from './components/Footer'
+import { fetchIP, fetchWeather } from './../../actions/weather';
 
 
 class LandingPage extends Component {
@@ -9,15 +14,36 @@ class LandingPage extends Component {
         super(props);
         this.state = {  };
     }
+
+    componentDidMount() {
+        this.props.fetchIP();
+    }
+
     render() {
         return (
             <Fragment>
                 <Navbar />
                 <WidgetBoard />
-                <WidgetWeather />
+                <WidgetWeather data={this.props.data} />
+                <WidgetFeatures />
+                <WidgetFooter />
             </Fragment>
         );
     }
 }
 
-export default LandingPage;
+LandingPage.propTypes = {
+    fetchIP: PropTypes.func.isRequired,
+    data: PropTypes.object.isRequired,
+    fetchWeather: PropTypes.func.isRequired
+}
+
+const mapStatetoProps = state => ({
+    data: state.weather
+});
+
+const mapDispatchToProps = {
+    fetchIP, fetchWeather
+};
+
+export default connect(mapStatetoProps, mapDispatchToProps)(LandingPage);
